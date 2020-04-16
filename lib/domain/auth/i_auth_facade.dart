61d16b3cb3b45c0d9f623243d8adb1b9130431fb
David <davidcobbina47@gmail.com>
2020-04-16 07:29:02 +0000

    added auth facade

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:googlekeepclone/domain/auth/value_objects.dart';
+
+import 'auth_failure.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
