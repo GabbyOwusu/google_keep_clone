202ca36f6accb2e60358a53ecb208134d5442d3e
David <davidcobbina47@gmail.com>
2020-04-16 08:16:20 +0000

    added initial setup for sign_in_bloc

---
+++ lib/application/auth/sign_in_form/bloc.dart
@@ -0,0 +1,1 @@
+export 'sign_in_form_bloc.dart';
---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:googlekeepclone/domain/auth/auth_failure.dart';
+import 'package:googlekeepclone/domain/auth/i_auth_facade.dart';
+import 'package:googlekeepclone/domain/auth/value_objects.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade);
+
+  @override
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: Add Logic
+  }
+}
